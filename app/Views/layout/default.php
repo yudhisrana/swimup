8b7d8472f0985f17678435184b8d80286bf4825f
verdip <dipoyudhisrana@gmail.com>
2025-06-14 22:55:53 +0700

    init layout

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -35,21 +35,15 @@
         <!-- footer -->
         <?= $this->include('components/footer'); ?>
         <!-- end footer -->
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="/assets/plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
-    <script src="/assets/plugin/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="/assets/plugin/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
     <script src="/assets/js/adminlte.js"></script>
 </body>
