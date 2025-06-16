b5d459f39410647d30d201ccd270b11ed67ae049
verdip <dipoyudhisrana@gmail.com>
2025-06-16 11:22:38 +0700

    add migration tabel user

---
+++ app/Database/Migrations/2025-06-16-041005_CreateUserTable.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUserTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'       => 'CHAR',
+                'constraint' => 36,
+                'null'       => false,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'username' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => true,
+            ],
+            'phone' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 15,
+                'null'       => true,
+            ],
+            'address' => [
+                'type'       => 'TEXT',
+                'null'       => true,
+            ],
+            'image' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => true,
+            ],
+            'status' => [
+                'type'       => 'BOOLEAN',
+                'null'       => false,
+                'default'    => true,
+            ],
+            'role_id' => [
+                'type'       => 'int',
+                'unsigned'   => true,
+                'null'       => false,
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+                'null'       => false,
+            ],
+            'updated_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addUniqueKey('username');
+        $this->forge->addUniqueKey('email');
+        $this->forge->addForeignKey('role_id', 'tbl_role', 'id', 'CASCADE', 'RESTRICT');
+        $this->forge->createTable('tbl_user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbl_user');
+    }
+}
